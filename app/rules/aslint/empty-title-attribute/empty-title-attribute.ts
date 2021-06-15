5fc2f84da2f38269504a0c4c093963d72295c8e0
Cezary Tomczyk <cezary.tomczyk@gmail.com>
2021-06-15 15:55:22 +0200

    Creating documentation for empty-title-attribute and adding unit tests

--- app/rules/aslint/empty-title-attribute/empty-title-attribute.ts
+++ app/rules/aslint/empty-title-attribute/empty-title-attribute.ts
@@ -2,12 +2,12 @@ import { CATEGORY_TYPE } from '../../../constants/categoryType';
 import { IIssueReport } from '../../../interfaces/rule-issue.interface';
 import { TextUtility } from '../../../utils/text';
 import { TranslateService } from '../../../services/translate';
-import { $severity } from '../../../constants/accessibility';
-import { $accessibilityAuditRules } from '../../../constants/accessibility';
+import { $accessibilityAuditRules, $severity } from '../../../constants/accessibility';
 import { AbstractRule, IAbstractRuleConfig } from '../../abstract-rule';
 
 export class EmptyTitleAttribute extends AbstractRule {
-  protected selector: string = `[title=""]:not(img)${[
+  protected selector: string = `[title]${[
+    ':not(img)',
     ':not(html)',
     ':not(head)',
     ':not(title)',
@@ -20,8 +20,7 @@ export class EmptyTitleAttribute extends AbstractRule {
     ':not(noscript)',
     ':not(iframe)',
     ':not(br)',
-    ':not(hr)',
-    ':not(:empty)'
+    ':not(hr)'
   ].join('')}`;
 
   protected ruleConfig: IAbstractRuleConfig = {
@@ -39,6 +38,12 @@ export class EmptyTitleAttribute extends AbstractRule {
 
   public validate(elements: Element[]): void {
     const reportEmptyTitle = (element: Element): void => {
+      const titleAttribute: string | null = element.getAttribute('title');
+
+      if (titleAttribute === null || titleAttribute.trim().length > 0) {
+        return;
+      }
+
       const report: IIssueReport = {
         message: TranslateService.instant('empty_title_attribute_report_message'),
         node: element,
